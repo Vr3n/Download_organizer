b0026285d581309af335e7b8413a4eccdb86f14e
Viren Patel <vickyspatel@gmail.com>
2019-11-03 13:13:46 +0530

    Extract file type from the Files

--- DownloadOrganizer.py
+++ DownloadOrganizer.py
@@ -15,13 +15,31 @@ dwnload_contents = os.listdir(current_dir)
 
 def main():
     count = 0
+    try:
+        for files in dwnload_contents:
+            count += 1
+            # print(f"{count}: {files}")
 
-    for files in dwnload_contents:
-        count += 1
-        print(f"{count}: {files}")
+            # Check if the Content is File or a Folder.
+            if os.path.isdir(files):
+                print("it's a Directory, skipping to next file.")
 
-    print(
-        f"*************************************************************\nThe number of files in this Directory are: {count}")
+            else:
+                # retrieving and storing the file type of files in kind variable.
+                kind = puremagic.ext_from_filename(files)
+
+                # Checks for the File type.
+                if kind is None:
+                    print("File type not recognized moving to 'Other's' Folder")
+
+                else:
+                    print(f"\n{count}: {files} is of type: {kind}")
+
+    except puremagic.PureError:
+        print("Cannot identyfy the File!!!")
+
+    # print(
+    #     f"*************************************************************\nThe number of files in this Directory are: {count}")
 
 
 if __name__ == "__main__":
